ef6969f25df7b8e692b1f364e5c06cbf1e1265f3
Meryem <meeryemtekeli@gmail.com>
2026-02-21 21:50:36 +0300

    Delete frontend directory

--- frontend/src/services/api/externalApi.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getRandomDogImage = async () => {
-  const res = await axios.get('https://dog.ceo/api/breeds/image/random');
-  return res.data.message;
-};
